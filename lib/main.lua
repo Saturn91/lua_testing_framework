8335b7f968328c6d00816b922716a93c456f1110
Manuel G <manuel.geissberger@gmail.com>
2022-08-02 23:24:21 +0200

    =[init]

---
+++ lib/main.lua
@@ -0,0 +1,6 @@
+test = require("lib/testlib")
+local t = require("test")
+
+test.init()
+t.run()
+test.result()
---
+++ tested_code.lua
@@ -0,0 +1,34 @@
+local code = {}
+
+function sort(a)
+	local temp = {}	
+	for i=1,#a do
+		temp[i] = a[i]
+	end
+    for i=1,#temp do
+        local j = i
+        while j > 1 and temp[j-1] > temp[j] do
+            temp[j],temp[j-1] = temp[j-1],temp[j]
+            j = j - 1
+        end
+    end
+	return temp;
+end
+
+function code.sort(a)
+    return (sort(a))
+end
+
+function wrap_int(int, min_i, max_i, add_i)
+	if not add_i then add_i = 0 end
+	local new_i = int+add_i
+	if new_i < min_i then return max_i end  
+	if new_i > max_i then return min_i end
+	return new_i
+end
+
+function code.wrap_int(int, min_i, max_i, add_i)
+    return wrap_int(int, min_i, max_i, add_i)
+end
+
+return code
